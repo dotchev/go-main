db86eaf05e9638200042521a19871a1e98988c97
Petar Dochev <petar.dochev@sap.com>
2018-05-21 19:06:59 +0300

    Add viper

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/dotchev/go-main/plugin"
 	echo "github.com/dotchev/go-plugin"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.ReadInConfig()
 	fmt.Println("Hello!")
 
 	r := plugin.PluginRequest{"main"}
