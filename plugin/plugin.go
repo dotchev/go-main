efa58bafe790ab27fcb1fd22b4ffd3917ffe02f9
Petar Dochev <petar.dochev@sap.com>
2018-05-22 15:10:45 +0300

    Add uuid really

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,6 +1,15 @@
 package plugin
 
+import (
+	uuid "github.com/satori/go.uuid"
+)
+
 type PluginRequest struct {
 	Name string
 	Size int
+}
+
+func UUID() string {
+	id := uuid.NewV4()
+	return id.String()
 }
